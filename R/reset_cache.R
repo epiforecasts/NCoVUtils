f35fa6ac2853860eaac08fc7cbf533feca2934bc
Hamish Gibbs <hamishgibbs@Hamishs-MBP.lan>
2020-03-30 12:21:53 +0100

    removed refresh data option

--- R/reset_cache.R
+++ R/reset_cache.R
@@ -8,46 +8,9 @@
 #'
 #'## Code
 #'reset_cache
-reset_cache <- function(refresh_data = FALSE) {
+reset_cache <- function() {
 
   unlink(".cache", recursive = TRUE)
 
-  cache <- memoise::cache_filesystem(".cache")
-
-  if (refresh_data) {
-    
-    tmp <- NCoVUtils::get_international_linelist()
-
-    tmp <- NCoVUtils::get_who_cases()
-
-    tmp <- NCoVUtils::get_italy_regional_cases()
-    
-    tmp <- NCoVUtils::get_ecdc_cases()
-    
-    tmp <- NCoVUtils::get_france_regional_cases()
-    
-    tmp <- NCoVUtils::get_germany_regional_cases()
-    
-    tmp <- NCoVUtils::get_interventions_data()
-    
-    tmp <- NCoVUtils::get_italy_regional_cases()
-    
-    tmp <- NCoVUtils::get_japan_regional_cases()
-    
-    tmp <- NCoVUtils::get_korea_regional_cases()
-    
-    tmp <- NCoVUtils::get_linelist()
-    
-    tmp <- NCoVUtils::get_spain_regional_cases()
-    
-    tmp <- NCoVUtils::get_total_cases()
-    
-    tmp <- NCoVUtils::get_uk_nhs_region_cases()
-    
-    tmp <- NCoVUtils::get_us_regional_cases()
-    
-  }
-
   return(invisible(NULL))
 }
-
