67e0b63997a29f7cb199442c30d8db45f4715b10
Hamish Gibbs <hamishgibbs@Hamishs-MBP.lan>
2020-03-30 09:57:39 +0100

    update reset_cache with new data sources

--- R/reset_cache.R
+++ R/reset_cache.R
@@ -15,12 +15,37 @@ reset_cache <- function(refresh_data = FALSE) {
   cache <- memoise::cache_filesystem(".cache")
 
   if (refresh_data) {
+    
     tmp <- NCoVUtils::get_international_linelist()
 
     tmp <- NCoVUtils::get_who_cases()
 
     tmp <- NCoVUtils::get_italy_regional_cases()
-
+    
+    tmp <- NCoVUtils::get_ecdc_cases()
+    
+    tmp <- NCoVUtils::get_france_regional_cases()
+    
+    tmp <- NCoVUtils::get_germany_regional_cases()
+    
+    tmp <- NCoVUtils::get_interventions_data()
+    
+    tmp <- NCoVUtils::get_italy_regional_cases()
+    
+    tmp <- NCoVUtils::get_japan_regional_cases()
+    
+    tmp <- NCoVUtils::get_korea_regional_cases()
+    
+    tmp <- NCoVUtils::get_linelist()
+    
+    tmp <- NCoVUtils::get_spain_regional_cases()
+    
+    tmp <- NCoVUtils::get_total_cases()
+    
+    tmp <- NCoVUtils::get_uk_nhs_region_cases()
+    
+    tmp <- NCoVUtils::get_us_regional_cases()
+    
   }
 
   return(invisible(NULL))
