aa8f825f3b5c936e545dd36c516fcc9e0a7d6db5
Hamish Gibbs <hamishgibbs@Hamishs-MBP.lan>
2020-04-01 13:13:38 +0100

    updated function, wrote test, updated docs

--- R/get_spain_regional_cases.R
+++ R/get_spain_regional_cases.R
@@ -4,10 +4,9 @@
 #' [Source](https://www.mscbs.gob.es/profesionales/saludPublica/ccayes/alertasActual/nCov-China/situacionActual.html)
 #' @return A dataframe of daily Spanish regional case counts.
 #' @importFrom pdftools pdf_text
-#' @importFrom dplyr select mutate row_number
+#' @importFrom dplyr select mutate row_number filter
 #' @importFrom memoise cache_filesystem memoise
-#' @importFrom stringr str_locate str_sub word str_remove_all
-#' @importFrom purrr walk
+#' @importFrom stringr str_locate str_sub word str_remove_all str_detect
 #' @export
 #' @examples
 #'
@@ -59,23 +58,24 @@
 #' }
 #' ##Code
 #' get_spain_regional_cases
-# get_spain_regional_cases <- function() {
-#
- # Path to table. Daily report 52 = 22 March 2020 so calculating daily updated link based on that
-link_1 <- "https://www.mscbs.gob.es/profesionales/saludPublica/ccayes/alertasActual/nCov-China/documentos/Actualizacion_"
-link_2 <- 52 + (as.numeric(Sys.Date()) - 18343)
-link_3 <- "_COVID-19.pdf"
-location <- paste0(link_1, link_2, link_3)
-#
-#   #set up cache
- ch <- memoise::cache_filesystem(".cache")
-mem_read <- memoise::memoise(purrr::walk, cache = ch)
-#
- # Read & clean data
-  mem_read(location, "es_covid.pdf", mode = "wb", quiet = TRUE)
-  text <- pdftools::pdf_text("es_covid.pdf")
+
+get_spain_regional_cases <- function() {
+
+  # Path to table. Daily report 52 = 22 March 2020 so calculating daily updated link based on that
+  link_1 <- "https://www.mscbs.gob.es/profesionales/saludPublica/ccayes/alertasActual/nCov-China/documentos/Actualizacion_"
+  link_2 <- 52 + (as.numeric(Sys.Date()) - 18343)
+  link_3 <- "_COVID-19.pdf"
+  location <- paste0(link_1, link_2, link_3)
+  
+  #set up cache
+  ch <- memoise::cache_filesystem(".cache")
+  mem_read <- memoise::memoise(pdftools::pdf_text, cache = ch)
+  
+  # Read & clean data
+  text <- mem_read(location)
+  
   p1 <- text[1]
-  rows <- as.matrix(scan(textConnection(p1), what="character", sep = "\n"))
+  rows <- suppressMessages(as.matrix(scan(textConnection(p1), what="character", sep = "\n")))
   start_row <- which(stringr::str_detect(rows, "Andalu"))
   end_row <- which(stringr::str_detect(rows, "La Rioja"))
   rows <- as.data.frame(rows)
@@ -86,13 +86,13 @@ mem_read <- memoise::memoise(purrr::walk, cache = ch)
       digit = stringr::str_locate(V1, "\\s[[:digit:]]")[,1],
       region = stringr::str_sub(V1, start = 1, end = (digit-1)),
       region = iconv(region, from = 'UTF-8', to = 'ASCII//TRANSLIT'),
+      region = trimws(region),
       cases = stringr::word(V1, -1),
       cases = stringr::str_remove_all(cases, "\\."),
       cases = as.numeric(cases) ) %>%
     dplyr::select(region, cases)
 
   return(data)
-#
-# }
+}
 
 
