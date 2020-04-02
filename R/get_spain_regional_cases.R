5e5d592f153cb758fa38ab4393fcdf53168d5a11
kathsherratt <62290797+kathsherratt@users.noreply.github.com>
2020-04-03 00:43:37 +0100

    Spain regional cases update
    New data source (same Spanish gov source but now available as a csv file) - so no additional dependencies.

--- R/get_spain_regional_cases.R
+++ R/get_spain_regional_cases.R
@@ -1,13 +1,12 @@
 #' Spain regional cases daily
 #'
 #' @description Extract regional spanish case counts.
-#' [Source](https://www.mscbs.gob.es/profesionales/saludPublica/ccayes/alertasActual/nCov-China/situacionActual.html)
+#' [Source](https://covid19.isciii.es/)
 #' @return A dataframe of daily Spanish regional case counts.
-#' @importFrom tabulizer extract_tables
-#' @importFrom dplyr select mutate
+#' @importFrom readr read_csv
+#' @importFrom dplyr select mutate filter left_join
 #' @importFrom memoise cache_filesystem memoise
-#' @importFrom stringr str_remove_all
-#' @importFrom tibble as_tibble
+#' @importFrom lubridate dmy
 #' @export
 #' @examples
 #'
@@ -24,18 +23,16 @@
 #' regions <- rnaturalearth::ne_states("Spain", returnclass = "sf")
 #'
 #' regions_with_data <- regions %>%
-#'   group_by(region) %>%
+#'   mutate(region_shortcode = stringr::str_remove_all(region_cod, "^ES\\."), # Match codes to dataset
+#'          region_shortcode = stringr::str_replace(region_shortcode, "^PM$", "IB"), # Baleares
+#'          region_shortcode = stringr::str_replace(region_shortcode, "^MU$", "MC"), # Murcia
+#'          region_shortcode = stringr::str_replace(region_shortcode, "^NA$", "NC"), # Navarra
+#'          region_shortcode = stringr::str_replace(region_shortcode, "^LO$", "RI"), # La Rioja
+#'          region_shortcode = ifelse(region == "Melilla", "ME", region_shortcode) # Melilla
+#'   ) %>%
+#'   group_by(region_shortcode) %>%
 #'   summarise(subregions = n(), do_union = TRUE) %>%
-#'   mutate(region = str_replace_all(region, "í", "i"),
-#'          region = str_replace_all(region, "ó", "o"),
-#'          region = str_replace_all(region, "ñ", "n"),
-#'          region = str_replace_all(region, "Foral de Navarra", "Navarra"),
-#'          region = str_replace_all(region, "Valenciana", "C. Valenciana"),
-#'          region = str_replace_all(region, "Canary Is\\.", "Canarias"),
-#'          region = str_replace_all(region, "Islas Baleares", "Baleares"),
-#'          region = str_replace_all(region, "-", " ")
-#'          ) %>%
-#'   left_join(data, by = c("region" = "region"))
+#'   left_join(data, by = c("region_shortcode" = "region"))
 #'
 #' # Map: Spain mainland + Baleares
 #' es_mainland <- ggplot2::ggplot(regions_with_data) +
@@ -59,30 +56,22 @@
 #' }
 #' ##Code
 #' get_spain_regional_cases
-# get_spain_regional_cases <- function() {
-#
-#   # Path to table. Daily report 52 = 22 March 2020 so calculating daily updated link based on that
-#   link_1 <- "https://www.mscbs.gob.es/profesionales/saludPublica/ccayes/alertasActual/nCov-China/documentos/Actualizacion_"
-#   link_2 <- 52 + (as.numeric(Sys.Date()) - 18343)
-#   link_3 <- "_COVID-19.pdf"
-#   location <- paste0(link_1, link_2, link_3)
-#
-#   #set up cache
-#   ch <- memoise::cache_filesystem(".cache")
-#   mem_read <- memoise::memoise(tabulizer::extract_tables, cache = ch)
-#
-#   # Extract & clean table (e.g. remove cumulative cases & incidence)
-#   out <- mem_read(location, pages = 1)
-#   cases <- do.call(rbind, out[length(out)])
-#   cases <- tibble::as_tibble(cases, .name_repair = "universal") %>%
-#     dplyr::select("region" = 1, "cases" = ncol(cases)) %>%
-#     dplyr::filter(region != "ESPAÃ‘A" & region != "CCAA" & region != "") %>%
-#     dplyr::mutate(region = iconv(region, from = 'UTF-8', to = 'ASCII//TRANSLIT'),
-#                   cases = as.character(cases),
-#                   cases = stringr::str_remove_all(cases, "[[:punct:]]"),
-#                   cases = as.numeric(cases))
-#   return(cases)
-#
-# }
+get_spain_regional_cases <- function() {
+  location <- "https://covid19.isciii.es/resources/serie_historica_acumulados.csv"
+# Set up cache
+  ch <- memoise::cache_filesystem(".cache")
+  mem_read <- memoise::memoise(readr::read_csv, cache = ch)
+# Read data & calculate daily change in cases
+  all_date <- suppressMessages(memoise::mem_read(location)) %>%
+    dplyr::mutate(date = lubridate::dmy(Fecha)) %>%
+    dplyr::select(region = 1, cases = 3, date)
+# Work out daily cases from cumulative totals
+  prev_date <- dplyr::filter(all_date, date == (max(date, na.rm=T)-1))
+  data <- dplyr::filter(all_date, date == max(date, na.rm=T)) %>%
+    dplyr::left_join(prev_date, by = c("region" = "region")) %>%
+    dplyr::mutate(new_cases = cases.x - cases.y) %>%
+    dplyr::select(region, new_cases)
+  return(data)
+}
 
 
