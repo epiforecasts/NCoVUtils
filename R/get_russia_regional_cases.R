1d5d9e9fbdf9c245c359fb32f8c3a94d58924fc2
kathsherratt <katharine.sherratt@lshtm.ac.uk>
2020-06-19 23:12:55 +0100

    update get_russia_regional_cases
    - Fix: data not updating
    - Fix: mapping to ISO codes (taken from Jonnie's code)

--- R/get_russia_regional_cases.R
+++ R/get_russia_regional_cases.R
@@ -18,14 +18,14 @@
 #'
 
 # # Mapping
-#' regions <- rnaturalearth::ne_states(geounit = "Russia", returnclass = "sf")
-#' data <- get_russia_regional_cases() %>%
-#'   dplyr::filter(date == max(date))
-#' regions_with_data <- dplyr::left_join(regions, data, by = "iso_3166_2")
-#' regions_with_data %>%
-#'   ggplot2::ggplot(ggplot2::aes(fill = cases)) +
-#'   ggplot2::geom_sf() +
-#'   ggplot2::coord_sf(xlim = c(20, max(sf::st_coordinates(regions_with_data))))
+# regions <- rnaturalearth::ne_states(geounit = "Russia", returnclass = "sf")
+# data <- get_russia_regional_cases() %>%
+#   dplyr::filter(date == max(date))
+# regions_with_data <- dplyr::left_join(regions, data, by = c("iso_3166_2" = "iso_code"))
+# regions_with_data %>%
+#   ggplot2::ggplot(ggplot2::aes(fill = cases)) +
+#   ggplot2::geom_sf() +
+#   ggplot2::coord_sf(xlim = c(20, max(sf::st_coordinates(regions_with_data))))
 #'   }
 
 get_russia_regional_cases <- function() {
@@ -40,7 +40,7 @@ get_russia_regional_cases <- function() {
 
   # Reshape
   russia <- russia %>%
-    tidyr::pivot_longer(cols = 12:133, names_to = "date") %>%
+    tidyr::pivot_longer(cols = 12:tidyr::last_col(), names_to = "date") %>%
     dplyr::select(date, country = Country_Region, region = Province_State, cases = value) %>%
     dplyr::mutate(date = lubridate::mdy(date))
 
@@ -52,15 +52,43 @@ get_russia_regional_cases <- function() {
     dplyr::ungroup()
 
   # Join to ISO region codes
-  iso_path <- "https://raw.githubusercontent.com/ZFTurbo/Covid-19-spread-prediction/master/input/russia_regions.csv"
-  iso <- suppressMessages(readr::read_csv(iso_path)) %>%
-    dplyr::select(iso_3166_2 = iso_code, region = csse_province_state) %>%
-    dplyr::mutate(
-      region = stringr::str_replace_all(region, "Republic of Altay", "Altay Republic"),
-      region = ifelse(iso_3166_2 == "RU-NEN", "Nenetskiy autonomous oblast", region),
-      region = ifelse(iso_3166_2 == "RU-CHU", "Chukotskiy autonomous oblast", region)
-      )
-  russia <- dplyr::left_join(russia, iso, by = "region")
+  get_russia_iso_codes <- function() {
+    region_url <- "https://en.wikipedia.org/wiki/ISO_3166-2:RU"
+    iso_table <- region_url %>%
+      xml2::read_html() %>%
+      rvest::html_nodes(xpath='//*[@id="mw-content-text"]/div/table') %>%
+      rvest::html_table(fill=TRUE)
+    iso_code <- iso_table[[1]][-1,]$Code
+
+    iso_codes <- tibble::tibble(
+      iso_code = c(iso_code, "UA-40", "UA-43"),
+      region = c("Adygea Republic", "Altai Republic", "Bashkortostan Republic", "Buryatia Republic",
+                 "Chechen Republic", "Chuvashia Republic", "Dagestan Republic", "Ingushetia Republic",
+                 "Kabardino-Balkarian Republic", "Kalmykia Republic", "Karachay-Cherkess Republic", "Karelia Republic",
+                 "Khakassia Republic", "Komi Republic", "Mari El Republic", "Mordovia Republic",
+                 "Sakha (Yakutiya) Republic", "North Ossetia - Alania Republic", "Tatarstan Republic",
+                 "Tyva Republic",  "Udmurt Republic",  "Altai Krai", "Kamchatka Krai",
+                 "Khabarovsk Krai",  "Krasnodar Krai", "Krasnoyarsk Krai", "Perm Krai", "Primorsky Krai", "Stavropol Krai",
+                 "Zabaykalsky Krai", "Amur Oblast", "Arkhangelsk Oblast", "Astrakhan Oblast", "Belgorod Oblast",
+                 "Bryansk Oblast", "Chelyabinsk Oblast",  "Irkutsk Oblast", "Ivanovo Oblast",
+                 "Kaliningrad Oblast", "Kaluga Oblast", "Kemerovo Oblast", "Kirov Oblast",
+                 "Kostroma Oblast", "Kurgan Oblast", "Kursk Oblast", "Leningrad Oblast",
+                 "Lipetsk Oblast", "Magadan Oblast", "Moscow Oblast", "Murmansk Oblast",
+                 "Nizhny Novgorod Oblast", "Novgorod Oblast", "Novosibirsk Oblast", "Omsk Oblast",
+                 "Orenburg Oblast",  "Orel Oblast",  "Penza Oblast", "Pskov Oblast",
+                 "Rostov Oblast", "Ryazan Oblast", "Sakhalin Oblast",  "Samara Oblast",
+                 "Saratov Oblast", "Smolensk Oblast", "Sverdlovsk Oblast",  "Tambov Oblast",
+                 "Tomsk Oblast", "Tula Oblast", "Tver Oblast",  "Tyumen Oblast",  "Ulyanovsk Oblast",
+                 "Vladimir Oblast", "Volgograd Oblast",  "Vologda Oblast",  "Voronezh Oblast",
+                 "Yaroslavl Oblast", "Moscow", "Saint Petersburg", "Jewish Autonomous Okrug", "Chukotka Autonomous Okrug",
+                 "Khanty-Mansi Autonomous Okrug", "Nenets Autonomous Okrug", "Yamalo-Nenets Autonomous Okrug", "Sevastopol",
+                 "Republic of Crimea"))
+
+    return(iso_codes)
+  }
+
+  iso_codes <- get_russia_iso_codes()
+  russia <- dplyr::left_join(russia, iso_codes, by = "region")
 
   return(russia)
 
